3e3f778eaca3e594f1ad40dd0283ec5b65159592
Fernando Rocha <beneditofernandor@gmail.com>
2023-08-26 14:29:47 -0300

    Configurando o AdminLTE

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,18 @@
-pàgina Admin
+@extends('adminlte::page')
+@section('title', 'Painel')
+@section('content_header')
+    <h1>painel de Controle</h1>
+@endsection
+@section('content')
+    ...
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="/asssets/css/style.css">
+@endsection
+
+@section('js')
+    <script>
+        alert('Rodando!');
+    </script>
+@endsection
