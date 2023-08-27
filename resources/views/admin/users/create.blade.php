ead747cb5f12946a7f29dd28ae363758fff4c7de
Fernando Rocha <beneditofernandor@gmail.com>
2023-08-27 13:08:15 -0300

    Usuários: Error no Input

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -11,57 +11,62 @@
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
-                <h4>Ocorreu um erro.</h4>
+                <h5><i class="icon fas fa-ban"></i>
+                    Ocorreu um erro.</h5>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
     @endif
+    <div class="card">
 
-    <form action="{{ route('users.store') }}" method="post" class="form-horizontal">
-        @csrf
-        <div class="form-group">
-            <div class="row">
-                <label for="inputEmail" class="col-sm-2 control-label">Nome Completo</label>
-                <div class="col-sm-10">
-                    <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+        <div class="card-body">
+            <form action="{{ route('users.store') }}" method="post" class="form-horizontal">
+                @csrf
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Nome Completo</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="name" value="{{ old('name') }}"
+                            class="form-control @error('name')is-invalid @enderror">
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label for="inputEmail" class="col-sm-2 control-label">E-mail</label>
-                <div class="col-sm-10">
-                    <input type="email" name="email" class="form-control" value="{{ old('email') }}"">
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label for="inputEmail" class="col-sm-2 control-label">Senha</label>
-                <div class="col-sm-10">
-                    <input type="password" name="password" class="form-control">
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label for="inputEmail" class="col-sm-2 control-label">Confirme sua senha</label>
-                <div class="col-sm-10">
-                    <input type="password" name="password_confirmation" class="form-control">
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label for="inputEmail" class="col-sm-2 control-label"></label>
-                <div class="col-sm-10">
-                    <input type="submit" name="cadastrar" class="btn btn-success" value="Cadastrar">
-                </div>
-            </div>
-        </div>
-    </form>
 
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" name="email" value="{{ old('email') }}"
+                            class="form-control
+                            @error('email')is-invalid @enderror">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password" class="form-control @error('password')is-invalid @enderror">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Confirme sua Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password_confirmation"
+                            class="form-control @error('password')is-invalid @enderror">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10">
+                        <input type="submit" value="Cadastrar" class="btn btn-success">
+                    </div>
+                </div>
+
+            </form>
+
+
+        </div>
+    </div>
 
 @endsection
